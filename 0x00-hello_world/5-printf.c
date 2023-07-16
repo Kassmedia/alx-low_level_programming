dcb7448e8a8adff291320538ed2938a53696e02b
Wahab <wabsid01@gmail.com>
2023-07-15 17:56:09 -0700

    add 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	printf("wth proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
