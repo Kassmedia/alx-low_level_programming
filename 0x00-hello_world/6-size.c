6c74173029dfab4b6ea1ea7e507dfbbd1a374343
Wahab <wabsid01@gmail.com>
2023-07-16 06:30:06 -0700

    love sa

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,8 @@ int main(void)
 {
 	printf("size of a char: %zu byte(s)\n", sizeof(char));
 	printf("size of a int: %zu byte(s)\n", sizeof(int));
-	printf("size of a short: %zu byte(s)\n", sizeof(short));
 	printf("size of a long: %zu byte(s)\n", sizeof(long));
 	printf("size of long long: %zu byte(s)\n", sizeof(long long));
 	printf("size of a float: %zu byte(s)\n", sizeof(float));
-	printf("size of a double: %zu bytes(s)\n", sizeof(float));
-	 printf("Size of long double: %zu byte(s)\n", sizeof(long double));
-
 	return (0);
 }
