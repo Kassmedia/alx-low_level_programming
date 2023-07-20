301a99fad209177749307d3dd59347a76ad0f724
Wahab <wabsid01@gmail.com>
2023-07-20 04:48:07 -0700

    sav num

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,10 @@
 */
 void print_numbers(void)
 {
-int number = '0';
-for (number = '0'; number < '9'; number++)
+int number = 48;
+for (number = 48; number < 58; number++)
 {
 _putchar(number);
 }
-_putchar("\n");
+_putchar(10);
 }
