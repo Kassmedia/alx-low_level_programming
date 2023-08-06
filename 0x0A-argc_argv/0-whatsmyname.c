95b57081c7a67710985f006791b167d145d15e02
Wahab <wabsid01@gmail.com>
2023-08-06 04:17:41 -0700

    sab

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
-int main(int argc __attribute__((unused)), char * argv[])
-	{
-		printf("%s\n", *argv[0]);
-		return (0);
-	}
+/**
+ * main - this is a function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
