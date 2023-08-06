1736fdb0b3cce5cd01f190f59076b13846b1e551
Wahab <wabsid01@gmail.com>
2023-08-06 04:41:14 -0700

    sav l

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-int main(int argc, char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc -1);
 	return (0);
