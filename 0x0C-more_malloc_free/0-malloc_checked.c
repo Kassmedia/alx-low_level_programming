5ea587bd28b2e5775bb3b70b0c59f560c0c3227a
Wahab <wabsid01@gmail.com>
2023-08-12 23:44:04 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * malloc_checked - allocate memory
  * @b: number of bytes
@@ -8,8 +9,10 @@
 void *malloc_checked(unsigned int b)
 {
 	void *c;
+
 	c = malloc(b);
 	if (c == NULL)
 		exit(98);
+
 	return(c);
 }
