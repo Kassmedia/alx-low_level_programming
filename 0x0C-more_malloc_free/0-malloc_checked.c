88643a7bba6ffba487b242a261e73ad5f215c79b
wahab <wabsid01@gmail.com>
2023-11-02 04:50:29 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - cause normal process termination with a status value of 98
+ *
+ * @b: allocated memory
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+    int *m = NULL;
+
+    m = malloc(sizeof(*m) * b);
+    if (m == NULL) {
+        exit(98);
+    }
+
+    return m;
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+more of malloc , calloc , and free function in C
