2a8ac5a2b5a811f1fba5a21a641cd70076c3a7a2
wahab <wabsid01@gmail.com>
2024-01-17 00:16:05 -0800

    100-operations.

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
-int add(int a, int b) {
-  return (a + b);
+int add(int a, int b) 
+{
+	return (a + b);
 }
 
-int sub(int a, int b) {
-  return (a - b);
+int sub(int a, int b) 
+{
+	return (a - b);
 }
 
-int mul(int a, int b) {
-  return (a * b);
+int mul(int a, int b) 
+{
+	return (a * b);
 }
 
-int div(int a, int b) {
-  return (a / b);
+int div(int a, int b) 
+{
+	return (a / b);
 }
 
-int mods(int a, int b) {
-  return (a % b);
+int mods(int a, int b) 
+{
+	return (a % b);
 }
