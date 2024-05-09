f398d23d58a2d387f21fc8b3709fd6cd1ba6d383
wahab <wabsid01@gmail.com>
2024-05-09 15:02:34 -0700

    103-exponential.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,14 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int binary_search_helper(int *array, int value,
+			 size_t low, size_t high);
+size_t min(size_t a, size_t b);
+int binary_search_helper(int *array, int value,
+			 size_t low, size_t high);
+int exponential_search(int *array, size_t size, int value);
+
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t l, size_t r);
 int binary_search_index(int *array, size_t l, size_t r, int value);
